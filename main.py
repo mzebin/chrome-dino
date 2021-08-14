aff9f1eae71ab7b362adb4f5e0fe9e79955ad5b1
mzebin <mohammed.zebin7@gmail.com>
2021-08-14 15:42:49 +0400

    Added Start Screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importing required modules.
 from itertools import cycle
+import os
 import random
 import sys
 
@@ -17,18 +18,77 @@ WHITE = (255, 255, 255)
 GREY = (83, 83, 83)
 
 # Creating fonts.
-font_path = "fonts/PressStart-2P-Regular.ttf"
+font_path = os.path.join("fonts", "PressStart2P-Regular.ttf")
 
 FONT_1 = pygame.font.Font(font_path, 48)
 FONT_2 = pygame.font.Font(font_path, 16)
 
 
 class Game:
+    DINO_START_IMAGE = pygame.image.load("images/dino/dino_start.png")
 
     def __init__(self):
+        # Scores.
+        self.score = 0
+        self.high_score = 0
+
+    def display_score(self):
+        score_text = "High Score: {} Score: {}".format(
+            self.high_score,
+            self.score,
+        )
+
+        score = FONT_2.render(score_text, True, GREY)
+        score_rect = score.get_rect()
+        score_rect.center = (950, 50)
+        SCREEN.blit(score, score_rect)
+
+    def start_screen(self, restart=True):
+        while True:
+            # Filling the screen with white colour.
+            SCREEN.fill(WHITE)
+
+            if restart:
+                # Creating restart message.
+                message = FONT_1.render("Press any key to restart", True, GREY)
+            else:
+                # Creating start message.
+                message = FONT_2.render("Press any key to start", True, GREY)
+
+            # Displaying Score.
+            self.display_score()
+
+            # Showing the image of the dino.
+            dino_image_rect = self.DINO_START_IMAGE.get_rect()
+            dino_image_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
+            SCREEN.blit(self.DINO_START_IMAGE, dino_image_rect)
+
+            # Showing the message.
+            message_rect = message.get_rect()
+            message_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+            SCREEN.blit(message, message_rect)
+
+            # Checking for events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                # Getting mouse button clicks.
+                clicks = pygame.mouse.get_pressed(3)
+                if event.type == pygame.KEYDOWN or clicks[0]:
+                    # Reset the score.
+                    self.score = 0
+
+                    # Running the mainloop
+                    self.mainloop()
+
+            # Updating the display.
+            pygame.display.update()
+
+    def mainloop(self):
         pass
 
 
 if __name__ == "__main__":
-    Game()
+    Game().start_screen()
 
